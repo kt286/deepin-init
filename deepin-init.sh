cce5d8a9c303c6a9d3b94a6702eb91a8517f4c04
Cloud <kt286@qq.com>
2018-01-25 14:16:22 +0800

    添加wine源

--- deepin-init.sh
+++ deepin-init.sh
@@ -20,6 +20,10 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
+#添加Wine源到source.list.d
+wget -q -O - https://dl.winehq.org/wine-builds/Release.key | sudo apt-key add -
+sudo sh -c 'echo "deb https://dl.winehq.org/wine-builds/debian/ DISTRO main" >> /etc/apt/sources.list.d/winehq.list'
+
 #卸载系统自带Flash（Chrome会自动更新的，不用Firefox）
 sudo apt-get purge libflashplugin-pepper
 rm -rf ~/.config/google-chrome/PepperFlash/
@@ -29,6 +33,7 @@ sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install google-chrome-stable #Chrome浏览器
 sudo apt-get install code #VSCode
 sudo apt-get install libappindicator3-1 #Lantern依赖，不安装的话会打不开蓝灯
+sudo apt-get install winehq-stable #wine deepin自带的那个有点旧
 
 #安装Lantren
 wget https://raw.githubusercontent.com/getlantern/lantern-binaries/master/lantern-installer-64-bit.deb
