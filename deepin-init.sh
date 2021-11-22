fb7682c9d92d86ca9ccb5637b3182d207ced37b4
Cloud <kt286@qq.com>
2021-11-22 17:26:21 +0800

    update to deepin 20.2.4

--- deepin-init.sh
+++ deepin-init.sh
@@ -3,12 +3,9 @@
 #先进到用户下载目录
 cd ~/Downloads
 
-#替换成163的源
-sudo sh -c 'echo "deb [by-hash=force] https://mirrors.cloud.tencent.com/deepin/ lion main contrib non-free" > /etc/apt/sources.list'
-
 #添加Chrome源到source.list.d
 wget -q -O - http://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list'
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 
 #添加VSCode源到source.list.d
 wget -q -O - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
@@ -16,35 +13,24 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode s
 
 #添加Docker源到source.list.d
 wget -q -O - https://mirrors.cloud.tencent.com/docker-ce/linux/debian/gpg | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://mirrors.cloud.tencent.com/docker-ce/linux/debian stretch stable" > /etc/apt/sources.list.d/docker.list'
+sudo sh -c 'echo "deb [arch=amd64] https://mirrors.cloud.tencent.com/docker-ce/linux/debian buster stable" > /etc/apt/sources.list.d/docker.list'
 
 #添加Typora源到source.list.d
 wget -q -O - https://typora.io/linux/public-key.asc | sudo apt-key add -
 sudo sh -c 'echo "deb https://typora.io/linux ./" > /etc/apt/sources.list.d/typora.list'
 
-#添加yarn源到source.list.d
-wget -q -O - https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://dl.yarnpkg.com/debian/ stable main" > /etc/apt/sources.list.d/yarn.list'
-
-#添加mysql源到source.list.d
-sudo apt-key adv --keyserver pgp.mit.edu --recv-keys A4A9406876FCBD3C456770C88C718D3B5072E1F5
-sudo sh -c 'echo "deb [arch=amd64] https://mirrors.cloud.tencent.com/mysql/apt/debian/ stretch mysql-8.0" > /etc/apt/sources.list.d/mysql.list'
-
-#卸载系统自带Flash（Chrome会自动更新的）
+#卸载系统自带Flash
 sudo apt-get purge -y libflashplugin-pepper
 rm -rf ~/.config/google-chrome/PepperFlash/
-
-#卸载自带QQ（原因不解释，我喜欢TIM）
-sudo apt-get purge -y deepin.com.qq.im
 
 #卸载深度帮助手册和深度欢迎两个没用还占地方的东西（会同时卸载dde，貌似没啥问题）
 sudo apt-get purge -y deepin-manual
 sudo apt-get purge -y dde-introduction
 
 #卸载自己不需要的软件
-sudo apt-get purge -y thunderbird
 sudo apt-get purge -y deepin-feedback
 sudo apt-get purge -y deepin-appstore*
+sudo apt-get purge -y deepin-app-store*
 sudo apt-get purge -y deepin-music
 sudo apt-get purge -y deepin-calculator
 sudo apt-get purge -y deepin-deepinid-client
@@ -55,45 +41,68 @@ sudo apt-get purge -y deepin-screensaver*
 sudo apt-get purge -y deepin-voice-recorder
 sudo apt-get purge -y deepin-screen-recorder
 sudo apt-get purge -y simple-scan
-sudo apt-get purge -y flatpak
 sudo apt-get purge -y plymouth-theme-deepin-logo
+sudo apt-get purge -y libreoffice*
+sudo apt-get purge -y deepin-draw
+sudo apt-get purge -y deepin-voice-note
+sudo apt-get purge -y deepin-album
+sudo apt-get purge -y deepin-camera
+sudo apt-get purge -y deepin-mail
+sudo apt-get purge -y fcitx*
+sudo apt-get purge -y org.deepin.browser
+
+#清理一下
+sudo apt-get autoremove -y --purge
 
 #安装软件
-sudo apt-get update && sudo apt-get dist-upgrade
+sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt-get install -y curl
+sudo apt-get install -y git
 sudo apt-get install -y console-setup
 sudo apt-get install -y deepin-screenshot
 sudo apt-get install -y deepin-image-viewer
 sudo apt-get install -y dde-calendar
 sudo apt-get install -y qt5-qmake
 sudo apt-get install -y code
-sudo apt-get install -y deepin.com.qq.office
-sudo apt-get install -y --no-install-recommends openjdk-8-jdk
+sudo apt-get install -y google-chrome-stable
+sudo apt-get install -y com.qq.office.deepin
+sudo apt-get install -y com.qq.weixin.deepin
 sudo apt-get install -y docker-ce
 sudo apt-get install -y typora
-sudo apt-get install -y --no-install-recommends yarn
-sudo apt-get install -y mysql-server
-
-#安装向日葵
-wget -t 3 -T 15 http://download.oray.com/sunlogin/linux/SunloginClient-10.0.2.24779_amd64.deb
-sudo dpkg -i SunloginClient-10.0.2.24779_amd64.deb
-
-#安装Lantren
-wget -t 3 -T 15 https://raw.githubusercontent.com/getlantern/lantern-binaries/master/lantern-installer-64-bit.deb
-sudo dpkg -i lantern-installer-64-bit.deb
+sudo apt-get install -y fcitx5-chinese-addons
+sudo apt-get install -y com.oray.sunlogin.client
+sudo apt-get install -y cn.wps.wps-office
 
 #删除Chrome推荐设置
 sudo rm /etc/opt/chrome/policies/recommended/*.json
 
 #更新TIM到最新版本
-sh -c  '/opt/deepinwine/apps/Deepin-TIM/run.sh -c'
+sh -c  '/opt/apps/com.qq.office.deepin/files/run.sh -c'
 export WINEPREFIX=$HOME/.deepinwine/Deepin-TIM
-rm TIM2.3.2.21173.exe
-wget -t 3 -T 15 https://dldir1.qq.com/qqfile/qq/TIM2.3.2/21173/TIM2.3.2.21173.exe
-deepin-wine TIM2.3.2.21173.exe
+rm TIM3.3.8.22043.exe
+wget -t 3 -T 15 https://dldir1.qq.com/qqfile/qq/PCTIM/TIM3.3.8/TIM3.3.8.22043.exe
+deepin-wine6-stable TIM3.3.8.22043.exe
+
+#更新微信到最新版本
+sh -c  '/opt/apps/com.qq.weixin.deepin/files/run.sh -c'
+export WINEPREFIX=$HOME/.deepinwine/Deepin-WeChat
+rm WeChatSetup.exe
+wget -t 3 -T 15 https://dldir1.qq.com/weixin/Windows/WeChatSetup.exe
+deepin-wine6-stable WeChatSetup.exe
 
 # 修改开机logo为详细信息（个人喜好）
 sudo plymouth-set-default-theme -R details
 
 #清理一下
-sudo apt-get autoremove --purge
+sudo apt-get autoremove -y --purge
+
+#设置fcitx图标(bloom主题缺少图标)
+sudo ln -s /usr/share/icons/bloom/apps/16/fcitx-kbd.svg /usr/share/icons/bloom/status/16/input-keyboard.svg
+sudo ln -s /usr/share/icons/bloom/apps/32/fcitx-kbd.svg /usr/share/icons/bloom/status/20/input-keyboard.svg
+sudo ln -s /usr/share/icons/bloom/apps/48/fcitx-kbd.svg /usr/share/icons/bloom/status/48/input-keyboard.svg
+sudo ln -s /usr/share/icons/bloom/apps/48/fcitx-kbd.svg /usr/share/icons/bloom/status/64/input-keyboard.svg
+
+sudo ln -s /usr/share/icons/bloom/apps/16/fcitx-kbd.svg /usr/share/icons/bloom/status/16/input-keyboard-symbolic.svg
+sudo ln -s /usr/share/icons/bloom/apps/32/fcitx-kbd.svg /usr/share/icons/bloom/status/20/input-keyboard-symbolic.svg
+sudo ln -s /usr/share/icons/bloom/apps/48/fcitx-kbd.svg /usr/share/icons/bloom/status/48/input-keyboard-symbolic.svg
+sudo ln -s /usr/share/icons/bloom/apps/48/fcitx-kbd.svg /usr/share/icons/bloom/status/64/input-keyboard-symbolic.svg
